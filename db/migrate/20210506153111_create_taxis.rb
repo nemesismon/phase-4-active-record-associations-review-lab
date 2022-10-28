b861dfce70bf5c5b4be9159953f0975f0df023a3
Michael <michael.w.kolb@gmail.com>
2022-10-28 13:30:47 -0400

    Completed assignment

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,7 +1,8 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
-
+      t.string :driver
+      t.integer :rides_count
       t.timestamps
     end
   end
